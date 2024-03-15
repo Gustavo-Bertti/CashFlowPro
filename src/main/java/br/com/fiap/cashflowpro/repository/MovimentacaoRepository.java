eaae3fb6c92e2794300f1b74a4158f37adbebd5b
Gustavo-Bertti <bertinhobertti@hotmail.com>
2024-03-15 11:55:37 -0300

    Alterações e inclusões de novas classes

---
+++ src/main/java/br/com/fiap/cashflowpro/repository/MovimentacaoRepository.java
@@ -0,0 +1,10 @@
+package br.com.fiap.cashflowpro.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.fiap.cashflowpro.model.Movimentacao;
+
+public interface MovimentacaoRepository extends JpaRepository<Movimentacao, Long> {
+
+
+}
